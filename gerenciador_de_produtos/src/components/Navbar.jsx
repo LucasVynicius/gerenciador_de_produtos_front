c25adc707dcaa29261f6c873014889963eeda9f9
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2024-11-21 10:52:53 -0300

    Crianção da Page Formulario de fornecedor

--- gerenciador_de_produtos/src/components/Navbar.jsx
+++ gerenciador_de_produtos/src/components/Navbar.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav className='menu'>
-        <div className='logo'>
-            <Link to="/">
-                <img src="/PRODUCT-manager-logo.png" alt="Logo do Sistema" className="logo-img" />
-            </Link>
-        </div>
-        <div className='menu-links'>
-            <Link to="/add-fornecedores">Adicionar Fornecedor</Link>
-            <Link to="/listar-fornecedores">Listar Fornecedores</Link>
-            <Link to="/add-produtos">Adicionar Produto</Link>
-            <Link to="/listar-produtos">Listar Produtos</Link>
-            <Link to="/add-clientes">Adicionar Cliente</Link>
-            <Link to="/listar-clientes">Listar Cliente</Link>
-        </div>
+    <nav className="menu">
+      <div className="logo">
+        <Link to="/">
+          <img
+            src="/PRODUCT-manager-logo.png"
+            alt="Logo do Sistema"
+            className="logo-img"
+          />
+        </Link>
+      </div>
+      <div className="menu-links">
+        <Link to="/add-fornecedores">Adicionar Fornecedor</Link>
+        <Link to="/listar-fornecedores">Listar Fornecedores</Link>
+        <Link to="/add-produtos">Adicionar Produto</Link>
+        <Link to="/listar-produtos">Listar Produtos</Link>
+        <Link to="/add-clientes">Adicionar Cliente</Link>
+        <Link to="/listar-clientes">Listar Clientes</Link>
+      </div>
     </nav>
-  )
-}
-
-export default Navbar
+  );
+};
+export default Navbar;
